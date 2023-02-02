10d980a813a96d8464e0f9ca7b7f2787be5eed4d
Muskaan-Maan <61053951+Muskaan-Maan@users.noreply.github.com>
2023-02-02 19:25:31 +0530

    Add files via upload

---
+++ Abstraction/StudentTester.java
@@ -0,0 +1,114 @@
+//----------------------------------------------------------------------------------------------------------------------
+//--------------------------------------ABSTRACTION - Exercise 1----------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+
+abstract class Student{
+
+    private String studentName;
+    private final int[] testScores = new int[4];
+    private String testResult;
+
+    public Student(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public abstract void generateResult();
+
+    public void setTestScore(int testNumber, int testScore) {
+        this.testScores[testNumber] = testScore;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int[] getTestScores() {
+        return testScores;
+    }
+
+    public String getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(String testResult) {
+        this.testResult = testResult;
+    }
+
+}
+class UndergraduateStudent extends Student{
+
+    public UndergraduateStudent(String studentName) {
+        super(studentName);
+    }
+
+    public void generateResult(){
+
+        int[] score = getTestScores();
+        int sum = 0;
+
+
+        for (int scores: score){
+            sum += scores;
+        }
+
+        int avg = sum / score.length;
+
+        if (avg >= 60){
+            setTestResult("Pass");
+        }else{
+            setTestResult("Fail");
+        }
+    }
+}
+class GraduateStudent extends Student{
+
+    public GraduateStudent(String studentName) {
+        super(studentName);
+    }
+
+    public void generateResult() {
+        int[] score = getTestScores();
+        int sum = 0;
+
+
+        for (int scores: score){
+            sum += scores;
+        }
+
+        int avg = sum / score.length;
+
+        if (avg >= 70){
+            setTestResult("Pass");
+        }else{
+            setTestResult("Fail");
+        }
+    }
+}
+public class StudentTester {
+    public static void main(String[] args) {
+        UndergraduateStudent undergraduateStudent = new UndergraduateStudent("Philip");
+        undergraduateStudent.setTestScore(0, 70);
+        undergraduateStudent.setTestScore(1, 69);
+        undergraduateStudent.setTestScore(2, 71);
+        undergraduateStudent.setTestScore(3, 55);
+        undergraduateStudent.generateResult();
+        System.out.println("Student name: "+undergraduateStudent.getStudentName());
+        System.out.println("Result: "+undergraduateStudent.getTestResult());
+        System.out.println();
+        GraduateStudent graduateStudent = new GraduateStudent("Jerry");
+        graduateStudent.setTestScore(0, 70);
+        graduateStudent.setTestScore(1, 69);
+        graduateStudent.setTestScore(2, 71);
+        graduateStudent.setTestScore(3, 55);
+        graduateStudent.generateResult();
+        System.out.println("Student name: "+graduateStudent.getStudentName());
+        System.out.println("Result : "+graduateStudent.getTestResult());
+
+    }
+}
+
+
